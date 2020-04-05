85936e5a96e73b5dbdd99c8d6bdd1808d64a1817
Wessel <wesselvdp@gmail.com>
2020-04-05 23:49:23 +0200

    added ga tracking

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,56 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import Link from "next/link";
 import Head from "next/head";
+import { initGA, logPageView } from "./GoogleAnalytics";
 
 type Props = {
   title?: string;
 };
-
+declare global {
+  interface Window {
+    GA_INITIALIZED: any;
+  }
+}
 const Layout: FC<Props> = ({
   children,
   title = "This is the default title",
-}) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;700&display=swap"
-        rel="stylesheet"
-      />
+}) => {
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }, []);
 
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header> </header>
-    <body
-      style={{
-        backgroundColor: "rgb(7,23,33)",
-        margin: "0",
-        padding: "15px",
-        color: "rgba(142,213, 198, 1)",
-        fontFamily: "Inconsolata, monospace",
-      }}
-    >
-      {children}
-    </body>
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
 
-    <footer></footer>
-  </div>
-);
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header> </header>
+      <body
+        style={{
+          backgroundColor: "rgb(7,23,33)",
+          margin: "0",
+          padding: "15px",
+          color: "rgb(142,213, 198)",
+          fontFamily: "Inconsolata, monospace",
+        }}
+      >
+        {children}
+      </body>
+
+      <footer></footer>
+    </div>
+  );
+};
 
 export default Layout;
